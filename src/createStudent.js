cbc1088fb0e977a6a747ea8854532120b33f9d1b
Douglas <cbdouglasvirissimo@hotmail.com>
2022-09-28 00:45:27 -0300

    5º Requisito

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const feedback = () => 'Eita pessoa boa!';
+
+const createStudent = (nome) => {
+  const estudante = {
+    name: nome, 
+    feedback,
+  };
+  return estudante;
+};
 
 module.exports = createStudent;
